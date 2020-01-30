fb492cce8837cc1fb27d594e2f4f4d5961a2d9cf
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-01-30 17:56:16 -0600

    Moving to laptop

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,27 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res){
+    burger.all(function(data){
+        var hbsObject = {
+            burger: data
+        };
+    res.render("index", hbsObject)
+    })
+})
 
+router.post("/", function(req, res){
+    burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result){
+        res.send("Post successful")
+    })
+})
+
+router.update("/", function(req, res){
+    burger.update()
+})
+
+router.delete("/", function(req, res){
+
+})
 
 module.exports = router
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express")
+
+var PORT = process.env.PORT || 8080;
+
+var app = express()
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Handlebars setup
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Require the router stuff and use it
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use(router);
+
+//Listener
+app.listen(PORT, function() {
+  console.log("I am listening, coder");
+});
