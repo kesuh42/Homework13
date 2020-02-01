f21482323a121da134a8b245a7c00d1a8c44f9dd
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-02-01 12:53:43 -0600

    heroku hookup

--- config/connection.js
+++ config/connection.js
@@ -9,14 +9,19 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("Error connecting to the database. Try again, coder");
-    console.log(err)
-    return;
-  }
-  console.log("Connection to database has been established, coder");
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+  connection.connect(function(err) {
+    if (err) {
+      console.error("Error connecting to the database. Try again, coder");
+      console.log(err)
+      return;
+    }
+    console.log("Connection to database has been established, coder");
+  });
+}
 
 // Export connection for the ORM to use.
 module.exports = connection;
